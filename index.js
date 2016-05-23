8b09cf2d64af7d2432b84daed3aee19f87c20c8b
Tomas Ruud <tomasruu@gmail.com>
2016-05-23 23:19:03 +0200

    Added work-section

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ var sass = require('node-sass');
 
 var language = 'no';
 
-Handlebars.registerHelper('get_network_class', function (network) {
-  return network.toLowerCase().replace(' ', '-');
-});
-
 function render(resume) {
 
   var css = sass.renderSync({
@@ -16,6 +12,34 @@ function render(resume) {
 
   var template = Handlebars.compile(fs.readFileSync('./resume.hbs', 'utf-8'));
   var translation = JSON.parse(fs.readFileSync('./i18n/' + language + '.json', 'utf-8'));
+
+  function format_date(date_string) {
+    var date = new Date(date_string);
+
+    if (!date)
+      return date_string;
+    var output = '';
+
+    return translation.months[date.getMonth()] + ' ' + date.getFullYear();
+  }
+
+  Handlebars.registerHelper('get_network_class', function (network) {
+    return network.toLowerCase().replace(' ', '-');
+  });
+
+  Handlebars.registerHelper('get_work_date', function (work) {
+    var date = "";
+
+    if (work.startDate)
+      date += format_date(work.startDate);
+
+    date += " &ndash; ";
+
+    if (work.endDate)
+      date += format_date(work.endDate);
+
+    return date;
+  });
 
   return template({
     css: '<style>' + css + '</style>',
